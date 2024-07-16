76b014a92d577a8f82aacd20d5b2687b66eb9477
Mateus <mateusfigueredo037@gmail.com>
2024-07-15 22:24:11 -0300

    fix comments on test

--- test/Cases/UserTest.php
+++ test/Cases/UserTest.php
@@ -26,58 +26,58 @@ class UserTest extends HttpTestCase
 
     protected $jwtSecretKey;
 
-    // public function testIndex()
-    // {
-    //     $res = $this->request('get', '/api/products');
-    //     $this->assertSame(200, $res->getStatusCode());
-    // }
+    public function testIndex()
+    {
+        $res = $this->request('get', '/api/products');
+        $this->assertSame(200, $res->getStatusCode());
+    }
 
-    // public function testListUsers()
-    // {
-    //     $faker = \Faker\Factory::create();
+    public function testListUsers()
+    {
+        $faker = \Faker\Factory::create();
 
-    //     $user = User::create(
-    //         [
-    //             'name' => 'test',
-    //             'email' => 'teste@teste.com',
-    //             'uuid' => Uuid::uuid4()->toString(),
-    //             'password' => password_hash('123456', PASSWORD_DEFAULT)
-    //         ]
-    //     );
+        $user = User::create(
+            [
+                'name' => 'test',
+                'email' => 'teste@teste.com',
+                'uuid' => Uuid::uuid4()->toString(),
+                'password' => password_hash('123456', PASSWORD_DEFAULT)
+            ]
+        );
 
-    //     $user2 = User::create(
-    //         [
-    //             'name' => $faker->name(),
-    //             'email' => $faker->email(),
-    //             'uuid' => Uuid::uuid4()->toString(),
-    //             'password' => password_hash('123456', PASSWORD_DEFAULT)
-    //         ]
-    //     );
+        $user2 = User::create(
+            [
+                'name' => $faker->name(),
+                'email' => $faker->email(),
+                'uuid' => Uuid::uuid4()->toString(),
+                'password' => password_hash('123456', PASSWORD_DEFAULT)
+            ]
+        );
 
-    //     $tokenPayload = [
-    //         'iss' => 'hyperf',
-    //         'iat' => time(),
-    //         'exp' => time() + 3600,
-    //         'sub' => $user->id,
-    //         'name' => $user->name,
-    //         'email' => $user->email,
-    //     ];
+        $tokenPayload = [
+            'iss' => 'hyperf',
+            'iat' => time(),
+            'exp' => time() + 3600,
+            'sub' => $user->id,
+            'name' => $user->name,
+            'email' => $user->email,
+        ];
 
-    //     $container = \Hyperf\Context\ApplicationContext::getContainer();
+        $container = \Hyperf\Context\ApplicationContext::getContainer();
 
-    //     $jwtSecretKey = $container->get(ConfigInterface::class);
-    //     $jwtSecretKey = $jwtSecretKey->get('jwt_secret_key');
+        $jwtSecretKey = $container->get(ConfigInterface::class);
+        $jwtSecretKey = $jwtSecretKey->get('jwt_secret_key');
 
-    //     $token = JWT::encode($tokenPayload, $jwtSecretKey, 'HS256');
+        $token = JWT::encode($tokenPayload, $jwtSecretKey, 'HS256');
 
-    //     $user->delete();
+        $user->delete();
 
-    //     $res = $this->client->get('/api/users', [], ['Authorization' => 'Bearer ' . $token]);
+        $res = $this->client->get('/api/users', [], ['Authorization' => 'Bearer ' . $token]);
 
-    //     $user2->delete();
+        $user2->delete();
 
-    //     $this->assertSame($user2->name, current($res)['name']);
-    // }
+        $this->assertSame($user2->name, current($res)['name']);
+    }
 
     public function testAlterPermission(){
     
